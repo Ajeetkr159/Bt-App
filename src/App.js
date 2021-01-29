f174c1a8a0a4eefb9348864543d6fc285ef42c09
Ajeet <ajeetkumar15999@gmail.com>
2021-01-29 09:34:34 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>hello Ajeet</h1>
+      <h1>my name is ajeet kumar</h1>
+      <h2>all about me</h2>
 
     </div>
   );
